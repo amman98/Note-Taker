68bd478876e1b8198b25ad4ae6b7c2873f2a0745
Amman Nega <ammannega@gmail.com>
2023-04-27 00:57:55 -0700

    added HTML route for notes.html

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+//const api = require('./routes/notes.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET route for notes page
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
