2cb22150f88acbbe1a4d6973211fc0560b99b6b2
Amman Nega <ammannega@gmail.com>
2023-04-27 20:44:18 -0700

    added DELETE route for notes

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ const notes = require("express").Router();
 const fs = require("fs");
 const util = require("util");
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 
 // GET Route returns all saved notes
 notes.get("/notes", (req, res) => {
@@ -31,6 +31,38 @@ notes.post("/notes", (req, res) => {
     } else {
         res.json('Error in posting note');
     }
+});
+
+// DELETE Route removes a note based on the id selected
+notes.delete("/notes/:id", (req, res) => {
+    const noteId = req.params.id; // grab id from api call
+    readFromFile("./db/db.json")
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            let hasId = false;
+            
+            // check if id exists in json
+            for(let i = 0; i < json.length; i++) {
+                if(json[i].id === noteId) {
+                    hasId = true;
+                    break;
+                }
+            }
+
+            //  write to file only if id exists
+            if(hasId) {
+                // create new array discluding the note with the id being deleted
+                const result = json.filter((note) => note.id !== noteId);
+
+                // write new resulting data to our json file
+                writeToFile('./db/db.json', result);
+
+                res.json(result);
+            }
+            else {
+                res.json('Error in deleting note'); // give error message as id isn't valid
+            }
+        })
 });
 
 module.exports = notes;
